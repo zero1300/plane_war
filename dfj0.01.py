f712b6a944f8b8c8f6e6c3dddbcd5a95fa706fd3
zero1300 <2806374351z@gamil.com>
2019-11-06 19:19:37 +0800

    第一次提交

---
+++ dfj0.01.py
@@ -0,0 +1,65 @@
+import pygame
+import random
+import sys
+import player
+from cons import *
+
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode([SCREEN_WIDTH, SCREEN_HIGH])
+    bg = pygame.image.load(r'C:\Users\hipaa\OneDrive\Pictures\636823110037182747.png')
+    plane1 = player.Plane(480//2-100 / 2, 800-97)
+    enemy1 = player.Enemy()
+    planebiulist = []
+    # jshuRect = pygame.Rect(plane1.x + 29, plane1.y, 21, 97)
+    # jhengRect = pygame.Rect(plane1.x, plane1.y + 37, 97, 23)
+    while True:
+        screen.blit(bg, (0, 0))
+        ran = random.randint(0, 400)
+        bullet1 = player.Bullet(plane1)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    plane1.direction = LEFT
+
+                elif event.key == pygame.K_RIGHT:
+                    plane1.direction = RIGHT
+
+                elif event.key == pygame.K_UP:
+                    plane1.direction = UP
+
+                elif event.key == pygame.K_DOWN:
+                    plane1.direction = DOWN
+
+        if ran < 201:
+            planebiulist.append(bullet1)
+        for fa in planebiulist:
+            fa.draw(screen)
+            if fa.x < 0:
+                planebiulist.remove(fa)
+        plane1.blit(screen)
+        enemy1.blit(screen)
+        if enemy1.live == 0:
+            out_string(screen, "爆炸", (0, 0, 0), (200, 200), 50)
+            print("dead")
+        pygame.display.update()
+        pygame.time.delay(100)
+        screen.fill([255, 255, 255])
+
+
+def out_string(screen, message, color, loc, size):
+    # 定义out_string函数,有四个参数
+    my_font = pygame.font.SysFont("宋体", size)
+    # 定义字体参数
+    t = my_font.render(message, True, color)
+    # 渲染字体赋值给t
+    screen.blit(t, loc)
+    # 输出t
+
+
+if __name__ == '__main__':
+    main()
+
